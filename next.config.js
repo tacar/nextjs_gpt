27fe9e55dd3845cc21007286798639114c0e3a78
tacar <tacarz@gmail.com>
2023-04-18 23:35:07 +0900

    first

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
+//require('dotenv').config();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
